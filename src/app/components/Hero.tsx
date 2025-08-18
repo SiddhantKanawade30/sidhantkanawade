27287c36e6a47fb4aba914477e93b2cac8bc2b09
Siddhant Kanawade <kanawadesiddhant30@gmail.com>
2025-08-18 15:16:26 +0530

    hero section done

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,47 @@
+export default function Hero() {
+    return (
+        <div className="absolute top-0 left-0 right-0 z-10 grid sm:grid-cols-[.7fr_0.3fr_12fr_0.3fr_.7fr] grid-cols-[.3fr_5fr_.35fr] sm:grid-cols-[2fr_0.3fr_12fr_0.3fr_2fr] grid-rows-6 sm:grid-rows-[1fr_4fr_8fr_1fr_3.5fr_1fr] sm:grid-rows-[1fr_4.5fr_4.5fr_.5fr_3.5fr_1fr] grid-rows-[1fr_2fr_3fr_.5fr_2fr_.5fr]">
+            <div className="hidden sm:block border-r border-gray-700 border-b"> 1</div>
+            <div className="sm:border-r border-gray-700 border-b ">2</div>
+            <div className="sm:border-r border-gray-700 border-b"> 3</div>
+            <div className="sm:border-r border-gray-700 border-b"> 4</div>
+            <div className="hidden sm:block border-r border-gray-700 border-b">5</div>
+            <div className="hidden sm:block border-r border-gray-700 border-b">6</div>
+            <div className="sm:border-r border-gray-700 border-b">7</div>
+            <div className="sm:border-r border-gray-700 border-b">8</div>
+            <div className="sm:border-r border-gray-700 border-b">9</div>
+            <div className="hidden sm:block border-r border-gray-700 border-b">10</div>
+            <div className="hidden sm:block border-r border-gray-700 border-b">11</div>
+            <div className="sm:border-r border-gray-700 border-b">12</div>
+            <div className="sm:border-r border-gray-700 border-b grid grid-cols-2 sm:grid-cols-[2.5fr_10fr] grid-cols-[5fr_8fr]">
+            
+            <div className="border-r border-gray-700 ">14</div>
+            <div className=" border-gray-700 grid grid-rows-2 grid-rows-[1fr_1fr]">
+                <div className=" sm:text-5xl text-3xl sm:mt-10 mt-7  border-gray-700 pl-1 "><h1>Hello</h1></div>
+                <div className="  pl-1 grid grid-rows-2 sm:grid-rows-[3fr_1fr] grid-rows-[2fr_1fr]">
+                    <div className="sm:text-6xl  sm:font-bold text-3xl font-semibold mt-1">I'm Siddhant</div>
+                    <div>Frontend dev</div>
+                </div>
+            </div>
+
+            </div>
+            <div className="sm:border-r border-gray-700 border-b">14</div>
+            <div className="hidden sm:block border-r border-gray-700 border-b">15</div>
+            <div className="hidden sm:block border-r border-gray-700 border-b">16</div>
+            <div className="sm:border-r border-gray-700 border-b">17</div>
+            <div className="sm:border-r border-gray-700 border-b text-xs items-center mt-1 pl-1">leaky code</div>
+            <div className="sm:border-r border-gray-700 border-b">19</div>
+            <div className="hidden sm:block border-r border-gray-700 border-b">20</div>
+            <div className="hidden sm:block border-r border-gray-700 border-b">21</div>
+            <div className="sm:border-r border-gray-700 border-b">22</div>
+            <div className="sm:border-r border-gray-700 border-b text-xs p-1 sm:text-base">Full-stack web developer specialized in JavaScript, working with React, Node.js, MongoDB, Tailwind, and TypeScript. I love solving problems and learning new technologies.</div>
+            <div className="sm:border-r border-gray-700 border-b">24</div>
+            <div className="hidden sm:block border-r border-gray-700 border-b">25</div>
+            <div className="hidden sm:block border-r border-gray-700 border-b bg-[repeating-linear-gradient(-45deg,transparent,transparent_3px,rgba(0,0,0,.3)_3px,rgba(0,0,0,.3)_4px)] ">26</div>
+            <div className="sm:border-r border-gray-700 border-b">27</div>
+            <div className="sm:border-r border-gray-700 border-b bg-[repeating-linear-gradient(-45deg,transparent,transparent_3px,rgba(0,0,0,.3)_3px,rgba(0,0,0,.3)_4px)]">28</div>
+            <div className="sm:border-r border-gray-700 border-b">29</div>
+            <div className="hidden sm:block border-r border-gray-700 border-b bg-[repeating-linear-gradient(-45deg,transparent,transparent_3px,rgba(0,0,0,.3)_3px,rgba(0,0,0,.3)_4px)]">30</div>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,27 @@
-import Image from "next/image";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <div>
-      <div className="grid grid-cols-[1fr_40fr_1fr] md:grid-cols-[2.5fr_0.3fr_10fr_0.3fr_2.5fr] grid-rows-[1fr_4fr_6fr_1.5fr] ">
+    <div className="relative min-h-screen ">
+      {/* Background Grid */}
+      <div className="absolute inset-0 z-0 bg-blue-500 grid grid-cols-[.5fr_0.3fr_12fr_0.3fr_.5fr] sm:grid-cols-[2fr_0.3fr_12fr_0.3fr_2fr]">
+        {/* Column 1 */}
+        <div className="border-r border-gray-700"></div>
+        
+        {/* Column 2 - with diagonal lines */}
+        <div className="border-r border-gray-700 sm:bg-[repeating-linear-gradient(-45deg,transparent,transparent_3px,rgba(0,0,0,.3)_3px,rgba(0,0,0,.3)_4px)]"></div>
+        
+        {/* Column 3 */}
+        <div className="border-r border-gray-700"></div>
+        
+        {/* Column 4 - with diagonal lines */}
+        <div className="border-r border-gray-700 sm:bg-[repeating-linear-gradient(-45deg,transparent,transparent_3px,rgba(0,0,0,.3)_3px,rgba(0,0,0,.3)_4px)]"></div>
+        
+        {/* Column 5 */}
+        <div></div>
 
-        {/* banner */}
-        <div className=" m-0 p-0 border-r-1  border-b-1 border-black ">1</div>
-        <div className="border-r-1  border-b-1 border-black ">2</div>
-        <div className="  border-r-1  border-b-1  border-black ">3</div>
-        <div className="  border-r-1  border-b-1 border-black">4</div>
-        <div className="  border-r-1  border-b-1">5</div>
-        <div className=" border-r-1  border-b-1 border-black ">6</div>
-        <div className=" border-r-1  border-b-1 border-black ">7</div>
-        <div className=" border-r-1  border-b-1 border-black ">8</div>
-        <div className=" border-r-1  border-b-1 border-black ">9</div>
-        <div className=" border-r-1  border-b-1 border-black ">10</div>
-        <div className=" border-r-1  border-b-1 border-black ">11 </div>
-        <div className=" border-r-1  border-b-1 border-black ">12</div>
-        <div className=" border-r-1  border-b-1 border-black grid grid-cols-[1fr_5fr]">
-          <div className="border-r-1  border-black">13</div>
-          <div className="  grid grid-rows-[1fr_1fr]">
-            
-            <div className="border-b text-5xl pt-6 pl-2">Hello</div>
-            <div className="text-5xl pt-6 pl-2"> I'm Siddhant </div>
-          </div>
-        </div>
-        <div className=" border-r-1  border-b-1 border-black ">14</div>
-        <div className=" border-r-1  border-b-1 border-black ">15</div>
-        <div className=" border-r-1  border-b-1 border-black ">16</div>
-        <div className=" border-r-1  border-b-1 border-black ">17</div>
-        <div className=" border-r-1  border-b-1 border-black grid grid-rows-[0.9fr_0.5fr]">
-          <div></div>
-          <div className="text-lg text-black text-sm m-0 p-0">text-lg text-white font-medium</div>
-        </div>
-        <div className=" border-r-1  border-b-1 border-black ">19</div>
-        <div className=" border-r-1  border-b-1 border-black ">20</div>
-      </div>
-
-      {/* profile section */}
-      {/* <div className="grid grid-cols-[1fr_40fr_1fr] md:grid-cols-[1fr_2fr_1fr] grid-rows-[10fr_2fr] md:grid-rows-[14fr_2fr] ">
-        <div className=" m-0 p-0 border-b-1 border-black ">1</div>
-        <div className=" border-b-1  border-r-1  border-l-1 grid grid-cols-[1.2fr_4fr]">
-          <div className="border-r-1">profile pic</div>
-          <div className="grid grid-rows-[3fr_2fr_1fr]">
-            <div className="border-b-1">leaky code</div>
-            <div className="border-b-1">name</div>
-            <div>popular posts</div>
-          </div>
-        </div>
-        <div className=" border-b-1  border-black ">3</div>
-        <div className=" m-0 p-0 border-b-1 border-black">4</div>
-        <div className=" border-b-1  border-r-1  border-l-1 ">5</div>
-        <div className=" border-b-1  border-black">6</div>
-    </div> */}
+      </div >
+      <Hero />
     </div>
   );
 }
