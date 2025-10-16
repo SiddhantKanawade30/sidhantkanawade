b23ed16528b66440c1d929e8119da9ab15d6c9a5
Siddhant Kanawade <kanawadesiddhant30@gmail.com>
2025-10-16 20:57:53 +0530

    delete: nextJS blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import { Container } from "./components/Container";
 import { Projects } from "./components/Projects";
 import { Hero } from "./components/Hero";
-// import { Current } from "./components/Current";
 import { LandingBlogs } from "./components/LandingBlogs";
 import { HeroWork } from "./components/HeroWork";
 import { Testimonials } from "./components/Testimonials";
