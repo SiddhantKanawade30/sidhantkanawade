406bb03b532628206ffd1903e38c2f1feda00b0e
Siddhant Kanawade <kanawadesiddhant30@gmail.com>
2025-11-22 04:15:37 +0530

    changed about heading

--- src/app/components/AboutHero.tsx
+++ src/app/components/AboutHero.tsx
@@ -9,15 +9,13 @@ export const AboutHero = () => {
         <div className="max-w-6xl mx-auto min-w-0">
           <Title>About Me</Title>
           <Subtitle>
-            I am a software engineer with a passion for building innovative products that help people live better lives and give me motivation to keep learning and growing.   
+            I am a 20 year old software engineer from India, fixing bugs I created.   
+            <br />
+            Open to freelance offers. Reach out to me to enquire more about my work and services.
           </Subtitle>
           
-          {/* Add more content sections here as needed */}
-          <div className="prose prose-lg max-w-none">
-            <p className="text-secondary text-sm md:text-md leading-relaxed">
-            I specialize in <span className="text-primary">Frontend Development</span>, creating modern and responsive web applications using React, Next.js, Tailwind CSS, and other cutting-edge technologies. I help businesses and individuals build high-performance digital solutions tailored to their goals, ensuring clean code, seamless user experiences, and optimal performance.
-            </p>
-          </div>
+          
+          
         </div>
       </Container>  
     )
